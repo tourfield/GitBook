c16cb20e8a17c8906d446a65acc79667adbb04e2
GearyYoung <ywzj0306@163.com>
2017-05-24 22:16:53 +0800

    Title: CustomerListView Context: 1.Create Fruit Class ``` public class Fruit {     private String name;     private int imageId;     public Fruit(String name,int imageId){         this.name=name;         this.imageId=imageId;     }     public String getName(){         return name;     }     public int getImageId(){         return imageId;     } } ``` 2. touch layout/fruit_item.xml ``` <?xml version="1.0" encoding="utf-8"?> <LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"     android:layout_width="match_parent"     android:layout_height="match_parent">     <ImageView         android:id="@+id/fruitImage"         android:layout_width="wrap_content"         android:layout_height="wrap_content" />     <TextView         android:id="@+id/fruitName"         android:layout_width="wrap_content"         android:layout_height="wrap_content"         android:layout_gravity="center_vertical"         android:layout_marginLeft="10dp"/> </LinearLayout> ``` 3. Create FruitAdapter class ``` public class FruitAdapter extends ArrayAdapter {     private int resourceId;     public FruitAdapter(Context context, int textViewResourceId, List<Fruit>obj){         super(context,textViewResourceId,obj);         resourceId=textViewResourceId;     }
        @NonNull
        @Override
        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
            Fruit fruit = (Fruit)getItem(position);
            View view = LayoutInflater.from(getContext()).inflate(resourceId,parent,false);
            ImageView fruitImage = (ImageView)view.findViewById(R.id.fruitImage);
            TextView fruitView = (TextView)view.findViewById(R.id.fruitName);
            fruitImage.setImageResource(fruit.getImageId());
            fruitView.setText(fruit.getName());
            return view;
        }
    }
    ```
    4. MainActivity
    ```
    public class MainActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener {
    	private List<Fruit> fruitList = new ArrayList<>();
        protected void onCreate(Bundle savedInstanceState) {
    	...
    	        if(actionBar!=null){
                actionBar.hide();
            }
            initFruit();
    //        ArrayAdapter <String> adapter = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_expandable_list_item_1,data);
            FruitAdapter fruitAdapter = new FruitAdapter(MainActivity.this,R.layout.fruit_iterm,fruitList);
    		listView=(ListView)findViewById(R.id.listView);
            listView.setAdapter(fruitAdapter);
    		listView.setOnItemClickListener(this);
    	}
        private void initFruit() {
            for (String fruitName:fruitNmaes){
                Fruit fruit = new Fruit(fruitName,R.drawable.cm);
                fruitList.add(fruit);
            }
        }
        @Override
        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
            Fruit fruit=fruitList.get(position);
            Toast.makeText(MainActivity.this,fruit.getName(),Toast.LENGTH_SHORT).show();
        }
    }
    ```

---
+++ app/src/main/java/com/github/tourfield/gitbook/Fruit.java
@@ -0,0 +1,20 @@
+package com.github.tourfield.gitbook;
+
+/**
+ * Created by Geary on 2017/5/24.
+ */
+
+public class Fruit {
+    private String name;
+    private int imageId;
+    public Fruit(String name,int imageId){
+        this.name=name;
+        this.imageId=imageId;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getImageId(){
+        return imageId;
+    }
+}
--- app/src/main/java/com/github/tourfield/gitbook/MainActivity.java
+++ app/src/main/java/com/github/tourfield/gitbook/MainActivity.java
@@ -9,12 +9,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener {
     private static final String TAG = "MainActivity";
     private Button gotoSelfBt;
     private Button gotSignInBt;
@@ -22,17 +28,71 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private ImageView imageView;
     private ProgressBar progressBar01;
     private ProgressBar progressBar02;
+    private ListView listView;
+    private String[] fruitNmaes={"peach桃子",
+            "Lemon 柠檬",
+            "Pear 梨子" ,
+            "avocado南美梨" ,
+            "cantaloupe美国香瓜" ,
+            "Banana 香蕉" ,
+            "Grape 葡萄" ,
+            "raisins葡萄干" ,
+            "plum 李子" ,
+            "apricot杏子",
+            "nectarine油桃" ,
+            "honeydew(melon)哈密瓜",
+            "orange 橙子",
+            "tangerine 橘子",
+            "guava番石榴",
+            "Golden apple 黄绿苹果、脆甜",
+            "Granny smith 绿苹果",
+            "papaya木瓜",
+            "Bramley绿苹果",
+            "Mclntosh麦金托什红苹果",
+            "coconut椰子 ",
+            "jack fruit 菠萝蜜、大树菠萝 ",
+            "prunes干梅子 ",
+            "blueberry 乌饭果 ",
+            "cranberry酸莓 ",
+            "raspberry山霉 ",
+            "Mango 芒果 ",
+            "fig 无花果 ",
+            "pineapple 菠萝 ",
+            "Kiwi 奇异果（弥猴桃） ",
+            "Star fruit 杨桃 ",
+            "Cherry 樱桃 ",
+            "watermelon西瓜" ,
+            "pumelo 柚子 ",
+            "lime 酸橙 ",
+            "Dates 枣子 ",
+            "lychee 荔枝 ",
+            "Grape fruit 葡萄柚 ",
+            "Coconut 椰子 ",
+            "Fig 无花果 ",
+            "Durin 榴梿 ",
+            "Loquat 枇杷 ",
+            "Pitaya 火龙果",
+            "strawberry草莓",
+            "orange橘子",
+            "Kumquat金桔",
+            "Raspberry覆盆子"
+    };
+    private List<Fruit> fruitList = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate: @ "+ this.toString());
         setContentView(R.layout.layout_main);
         ActionBar actionBar = getSupportActionBar();
-        if(actionBar!=null)
-        {
+        if(actionBar!=null){
             actionBar.hide();
         }
+        initFruit();
+//        ArrayAdapter <String> adapter = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_expandable_list_item_1,data);
+        FruitAdapter fruitAdapter = new FruitAdapter(MainActivity.this,R.layout.fruit_iterm,fruitList);
         gotoSelfBt=(Button)findViewById(R.id.gotoSelf);
+        listView=(ListView)findViewById(R.id.listView);
+        listView.setAdapter(fruitAdapter);
         gotSignInBt=(Button)findViewById(R.id.gotoSignIn);
         imageView = (ImageView)findViewById(R.id.draginCatImg);
         progressBar01 = (ProgressBar)findViewById(R.id.progressBar01);
@@ -40,6 +100,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         gotoSelfBt.setOnClickListener(this);
         gotSignInBt.setOnClickListener(this);
         imageView.setOnClickListener(this);
+        listView.setOnItemClickListener(this);
+    }
+
+    private void initFruit() {
+        for (String fruitName:fruitNmaes){
+            Fruit fruit = new Fruit(fruitName,R.drawable.cm);
+            fruitList.add(fruit);
+        }
     }
 
     @Override
@@ -57,8 +125,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.draginCatImg:
 //                Toast.makeText(this,"show",Toast.LENGTH_SHORT).show();
 //                imageView.setImageResource(R.drawable.flowers);
-//                if(imageView.getVisibility()==View.VISIBLE)
-//                    imageView.setVisibility(View.GONE);
+                if(imageView.getVisibility()==View.VISIBLE)
+                    imageView.setVisibility(View.GONE);
                 AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
                 dialog.setTitle("Show Dialog");
                 dialog.setMessage("ARE YOU Sure to disable progress Bar!");
@@ -92,5 +160,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
         }
 
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Fruit fruit=fruitList.get(position);
+        Toast.makeText(MainActivity.this,fruit.getName(),Toast.LENGTH_SHORT).show();
     }
 }
