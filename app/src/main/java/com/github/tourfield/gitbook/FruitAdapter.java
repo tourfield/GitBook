436f0f68217c383e3b5d2533994a2dd3992b836b
GearyYoung <ywzj0306@163.com>
2017-05-25 22:20:46 +0800

    TAG #V0.07 RecyclerView组件纵向滚动使用
    [Context    ]:RecyclerView

--- app/src/main/java/com/github/tourfield/gitbook/FruitAdapter.java
+++ app/src/main/java/com/github/tourfield/gitbook/FruitAdapter.java
@@ -3,6 +3,7 @@ package com.github.tourfield.gitbook;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +19,10 @@ import java.util.List;
 
 public class FruitAdapter extends ArrayAdapter {
     private int resourceId;
+    class ViewHolder{
+        ImageView fruitImage;
+        TextView fruitName;
+    }
     public FruitAdapter(Context context, int textViewResourceId, List<Fruit>obj){
         super(context,textViewResourceId,obj);
         resourceId=textViewResourceId;
@@ -27,11 +32,25 @@ public class FruitAdapter extends ArrayAdapter {
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         Fruit fruit = (Fruit)getItem(position);
-        View view = LayoutInflater.from(getContext()).inflate(resourceId,parent,false);
-        ImageView fruitImage = (ImageView)view.findViewById(R.id.fruitImage);
-        TextView fruitView = (TextView)view.findViewById(R.id.fruitName);
-        fruitImage.setImageResource(fruit.getImageId());
-        fruitView.setText(fruit.getName());
+        View view;
+        ViewHolder viewHolder;
+        if(convertView == null) {
+            view = LayoutInflater.from(getContext()).inflate(resourceId, parent, false);
+            viewHolder = new ViewHolder();
+            viewHolder.fruitImage = (ImageView)view.findViewById(R.id.fruitImage);
+            viewHolder.fruitName=(TextView)view.findViewById(R.id.fruitName);
+            view.setTag(viewHolder);
+        }
+        else {
+            view = convertView;
+            viewHolder=(ViewHolder)view.getTag();
+        }
+//        ImageView fruitImage = (ImageView)view.findViewById(R.id.fruitImage);
+//        TextView fruitView = (TextView)view.findViewById(R.id.fruitName);
+//        fruitImage.setImageResource(fruit.getImageId());
+//        fruitView.setText(fruit.getName());
+        viewHolder.fruitImage.setImageResource(fruit.getImageId());
+        viewHolder.fruitName.setText(fruit.getName());
         return view;
     }
 }
---
+++ app/src/main/java/com/github/tourfield/gitbook/FruitRecyclerAdapter.java
@@ -0,0 +1,68 @@
+package com.github.tourfield.gitbook;
+
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+/**
+ * Created by Geary on 2017/5/24.
+ */
+
+public class FruitRecyclerAdapter extends RecyclerView.Adapter<FruitRecyclerAdapter.ViewHolder> implements View.OnClickListener {
+    private static final String TAG = "FruitRecyclerAdapter";
+    private List<Fruit> mFruitList;
+    private ViewHolder holder;
+
+    @Override
+    public void onClick(View v) {
+        int position = holder.getAdapterPosition();
+        Fruit fruit = mFruitList.get(position);
+        Toast.makeText(v.getContext(),"fruitImage"+fruit.getName(),Toast.LENGTH_SHORT).show();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView fruitImage;
+        TextView fruitName;
+        public ViewHolder(View view){
+            super(view);
+            Log.d(TAG, "ViewHolder: @ "+ this.toString());
+            fruitImage=(ImageView)view.findViewById(R.id.fruitImage);
+            fruitName = (TextView)view.findViewById(R.id.fruitName);
+        }
+    }
+    public FruitRecyclerAdapter(List<Fruit> fruitList){
+        mFruitList = fruitList;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Log.d(TAG, "onCreateViewHolder: @ "+ this.toString());
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.fruit_iterm,parent,false);
+        Log.d(TAG, "before call : ViewHolder");
+        holder= new ViewHolder(view);
+        Log.d(TAG, "after call : ViewHolder");
+        holder.fruitImage.setOnClickListener(this);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Log.d(TAG, "onBindViewHolder: @ "+ position);
+        Fruit fruit = mFruitList.get(position);
+        holder.fruitImage.setImageResource(fruit.getImageId());
+        holder.fruitName.setText(fruit.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        Log.d(TAG, "getItemCount: @ "+ mFruitList.size());
+        return mFruitList.size();
+    }
+}
